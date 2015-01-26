10ebef942ba5d3e9082a12167fa8fc72836b8bb2
Cody Lee <cody117@gmail.com>
2015-01-26 17:10:31 -0600

    test version increment

--- jquery.boldviewer.js
+++ jquery.boldviewer.js
@@ -1,11 +1,11 @@
 //jquery.boldviewer.js
 
-/*! BoldViewer @version 0.0.1-12 | Bold Innovation Group | MIT License | github.com/BOLDInnovationGroup/image-viewer */
+/*! BoldViewer @version 0.0.1-13 | Bold Innovation Group | MIT License | github.com/BOLDInnovationGroup/image-viewer */
 
 ;( function ( window, document, $, undefined ) {
 
 	$.boldviewer = function( elem, options ) {
         
-    };
+    }; 
     
 }( window, document, jQuery ) );
